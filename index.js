ae3c8711dbc146900f3fb7ea8f92a88e3ef62ace
gogy <pravdin.ev97@gmail.com>
2020-07-29 13:45:56 +0300

    migrate to async functions

--- index.js
+++ index.js
@@ -2,55 +2,66 @@ const redis = require('redis');
 const { promisify } = require("util");
 const { isError, getMessage } = require('./utils');
 const client = redis.createClient();
+
+// async functions
 const lrangeAsync = promisify(client.lrange).bind(client);
 const delAsync = promisify(client.del).bind(client);
+const clientAsync = promisify(client.client).bind(client);
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+const lpushAsync = promisify(client.lpush).bind(client);
+const rpopAsync = promisify(client.rpop).bind(client);
 
+// queues
 const QUEUE = 'messages_queue';
 const INCORRECT = 'incorrects';
 
-if (process.argv.includes('getErrors')) {
-  lrangeAsync(INCORRECT, 0, -1)
-    .then((res) => {
-      console.log(res);
-      delAsync(INCORRECT)
-        .then((res) => {
-          console.log('DELETE SUCCESS');
-          process.exit(0)
-        })
-    });
-} else {
-  let id;
+
+async function start() {
+  process.argv.includes('getErrors') ? getErrors() : standard();
+} 
+
+start();
+
+
+async function getErrors() {
+  const errors = await lrangeAsync(INCORRECT, 0, -1);
+  console.log(errors);
+  await delAsync(INCORRECT);
+  console.log('DELETE SUCCESS');
+  process.exit(0);
+}
+
+async function standard() {
+  let id = await clientAsync('id');
   let exitMode = receiverMode();
-  client.client('id', (err, res) => id = res);
 
   client.on('error', (err) => {
     console.error('Error: ', err);
   });
 
-
-  setInterval(() => client.client('list', (err, msg) => {
-    const clientIds = msg.split('\n').filter(Boolean).map((item) => item.split(' ')[0].substring(3));
-    client.get('generatorId', (err, res) => {
-      if (!res || !clientIds.includes(res)) {
-        client.set('generatorId', id);
-        exitMode();
-        exitMode = generatorMode();
-        return;
-      }
-    });
-  }), 1);
-  
+  // check generator is working
+  setInterval(async () => {
+    const list = await clientAsync('list');
+    const clientIds = list.split('\n').filter(Boolean).map((item) => item.split(' ')[0].substring(3));
+    const generatorId = await getAsync('generatorId');
+    if (!generatorId || !clientIds.includes(generatorId)) {
+      await setAsync('generatorId', id);
+      exitMode();
+      exitMode = generatorMode();
+      return;
+    }
+  }, 1);
 }
 
 function receiverMode() {
-  const receiver = setInterval(() => {
-    client.rpop(QUEUE, (err, msg) => {
-      if (!msg) return;
-      console.log('Message: ', msg);
-      if (isError()) {
-        client.lpush(INCORRECT, msg);
-      }
-    });
+  const receiver = setInterval(async () => {
+    const msg = await rpopAsync(QUEUE);
+    if (!msg) return;
+    console.log('Message: ', msg);
+    if (isError()) {
+      await lpushAsync(INCORRECT, msg);
+    }
   }, 0);
 
   return () => clearInterval(receiver);
@@ -58,7 +69,7 @@ function receiverMode() {
 
 function generatorMode() {
   const generator = setInterval(() => {
-    client.lpush(QUEUE, getMessage());
+    lpushAsync(QUEUE, getMessage());
   }, 1000);
 
   return () => clearInterval(generator);
